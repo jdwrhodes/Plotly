ee7c194cdce390ad5d20cadb489346f83d4362ae
Joshua Rhodes <jdw.rhodes@gmail.com>
2021-03-22 21:12:31 -0500

    Added game.js

--- Module_work/static/javascript/game.js
+++ Module_work/static/javascript/game.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300},
+            debug: true
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -21,9 +28,71 @@ function preload() {
 }
 
 function create() {
-    this.add.image(400, 300, 'background')
+    this.add.image(400, 300, 'background');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400,600, 'platform').setScale(2).refreshBody();
+
+    platforms.create(Math.floor(Math.random() * 400), Math.floor(Math.random() * 600), 'platform');
+    platforms.create(Math.floor(Math.random() * 400), Math.floor(Math.random() * 300), 'platform');
+
+    player = this.physics.add.sprite(100, 450, 'rollman');
+
+    player.setBounce(0.2);
+
+    player.setCollideWorldBounds(true)
+
+    this.physics.add.collider(player, platforms);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('rollman', {frames:[8,7,6,5,4,3,2,1,0]}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('rollman', {start: 15, end: 23}),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    this.anims.create({
+        key:'default',
+        frames: [{key: 'rollman', frame: 9}],
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key:'up',
+        frames: this.anims.generateFrameNumbers('rollman', {start: 12, end: 9}),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+        player.anims.play('default', true);
+    }
+
+    if (cursors.up.isDown){
+        player.setVelocityY(-300);
+        player.anims.play('up', true);
+    }
 
 }
